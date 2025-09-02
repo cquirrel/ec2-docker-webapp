41884e555b263bce0e7b32367f7a945f713e02eb
cquirrel <9157825+cquirrel@users.noreply.github.com>
2025-09-02 14:34:20 +0100

    Testing swarm mode

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker stack rm stackdemo
+docker stack rm ec2-docker-swarm
 sleep 10
-docker volume rm stackdemo_db-data
+docker volume rm ec2-docker-swarm_db-data
 docker compose build
 docker compose push
-docker stack deploy --compose-file compose.yaml stackdemo
+docker stack deploy --compose-file compose.yaml ec2-docker-swarm
